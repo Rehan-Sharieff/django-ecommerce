f48b1b462d6f9d18385a20ace91e378afa8828c9
Rehan Sharieff <darksoul.darkavatar@gmail.com>
2025-12-31 13:28:07 +0530

    Initial Django e-commerce project

---
+++ store/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def _str_(self):
+        return self.name
+# Create your models here.
